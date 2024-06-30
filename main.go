1c87fe6a37bcbdf279e475c0357d4a3873956e96
glorious73 <amjadmh73@gmail.com>
2024-06-30 20:06:39 +0300

    more functions

--- main.go
+++ main.go
@@ -15,19 +15,9 @@ func main() {
 	var bookings []string
 
 	for remainingTickets > 0 {
-		var firstName string
-		var lastName string
-		var userTickets uint
 
-		fmt.Println("Please enter your first name")
-		fmt.Scan(&firstName)
-		fmt.Println("Please enter your last name")
-		fmt.Scan(&lastName)
-		fmt.Println("Please enter number of tickets to be booked")
-		fmt.Scan(&userTickets)
-
-		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-		var isValidTickets bool = userTickets > 0 && userTickets <= remainingTickets
+		firstName, lastName, userTickets := getUserInput()
+		isValidName, isValidTickets := validateInput(firstName, lastName, userTickets, remainingTickets)
 
 		if isValidName && isValidTickets {
 			remainingTickets = remainingTickets - userTickets
@@ -58,4 +48,25 @@ func getFirstNames(bookings []string) []string {
 		firstNames = append(firstNames, names[0])
 	}
 	return firstNames
+}
+
+func getUserInput() (string, string, uint) {
+	var firstName string
+	var lastName string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Please enter number of tickets to be booked")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, userTickets
+}
+
+func validateInput(firstName string, lastName string, userTickets uint, remainingTickets uint) (bool, bool) {
+	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+	var isValidTickets bool = userTickets > 0 && userTickets <= remainingTickets
+	return isValidName, isValidTickets
 }
