5932bc1079f89d9c7bcb4a5e88f6518dd7c3ac40
glorious73 <amjadmh73@gmail.com>
2024-06-30 19:54:37 +0300

    improved condtion checks

--- main.go
+++ main.go
@@ -27,9 +27,11 @@ func main() {
 		fmt.Scan(&lastName)
 		fmt.Println("Please enter number of tickets to be booked")
 		fmt.Scan(&userTickets)
-		fmt.Printf("%v booked %v tickets.\n", firstName, userTickets)
 
-		if userTickets <= remainingTickets {
+		var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
+		var isValidTickets bool = userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidTickets {
 			remainingTickets = remainingTickets - userTickets
 			fmt.Printf("Remaining tickets for conference are %v\n", remainingTickets)
 
@@ -39,9 +41,10 @@ func main() {
 				var names = strings.Fields(booking)
 				firstNames = append(firstNames, names[0])
 			}
+			fmt.Printf("%v booked %v tickets.\n", firstName, userTickets)
 			fmt.Printf("First names are %v\n", firstNames)
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets.\n", remainingTickets, userTickets)
+			fmt.Printf("First name and last name each contain at least 2 characters and the remaining # of tickets is %v\n", remainingTickets)
 			continue
 		}
 	}
