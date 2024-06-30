7771003dda64b6202055f82eb3d3c85ff6d0a738
glorious73 <amjadmh73@gmail.com>
2024-06-30 17:54:56 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+	const totalTickets = 50
+	var remainingTickets uint = 50
+
+	var bookings [50]string
+
+	fmt.Println("Welcome to", conferenceName, "booking application!")
+	fmt.Println("We have a total of", totalTickets, "and we have", remainingTickets, "remaining.")
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Please enter number of tickets to be booked")
+	fmt.Scan(&userTickets)
+	fmt.Printf("%v booked %v tickets.\n", firstName, userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+	fmt.Printf("Remaining tickets for conference are %v\n", remainingTickets)
+
+	bookings[0] = firstName
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array type: %T\n", bookings[0])
+	fmt.Printf("Array length: %v\n", len(bookings))
+}
